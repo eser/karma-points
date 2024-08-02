2cfb76d4df6239aa6ba7195c4fdca4874e033873
Samet Erkan Boz <71569375+sameterkanboz@users.noreply.github.com>
2024-08-02 16:24:17 +0300

    feat: adding air to backend

--- backend/cmd/karmapoints/main.go
+++ backend/cmd/karmapoints/main.go
@@ -9,6 +9,13 @@ import (
 
 func main() {
 	fmt.Println("Starting server on :8080")
-	http.HandleFunc("/", routes.HomeHandler)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", routes.HomeHandler)
+	mux.HandleFunc("GET /api/notes", routes.NotesHandler)
+
+	mux.HandleFunc("POST /api/notes", routes.CreateNoteHandler)
+
+	http.ListenAndServe(":8080", mux)
+
 }
--- backend/pkg/karmapoints/routes/routes.go
+++ backend/pkg/karmapoints/routes/routes.go
@@ -8,3 +8,11 @@ import (
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to Karma Points!")
 }
+
+func NotesHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Notes API")
+}
+
+func CreateNoteHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create Note API")
+}
